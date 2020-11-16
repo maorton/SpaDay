18940eefca4cd286b993315a63db79498cc71484
Megan Orton <megan.orton0@gmail.com>
2020-11-15 21:42:54 -0600

    added new user functions

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (verify == newUser.password)
+            {
+                ViewBag.name = newUser.username;
+                return View("Index");
+            }
+             else
+            {
+                ViewBag.name = newUser.username;
+                ViewBag.email = newUser.email;
+                ViewBag.error = "The password does not match";
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public User(string Username, string Email, string Password)
+        {
+            username = Username;
+            email = Email;
+            password = Password;
+        }
+
+        public User()
+        {
+
+        }
+    }
+
+   
+}
